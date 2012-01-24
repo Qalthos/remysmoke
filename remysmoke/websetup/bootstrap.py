f7aa0281b2d4d515150aebff73d1590a8474e8d8
Nathaniel Case <Qalthos@gmail.com>
2012-01-24 14:00:50 -0500

    Modify the default permissions setup.

--- remysmoke/websetup/bootstrap.py
+++ remysmoke/websetup/bootstrap.py
@@ -17,31 +17,32 @@ def bootstrap(command, conf, vars):
         u.display_name = u'Example manager'
         u.email_address = u'manager@somedomain.com'
         u.password = u'managepass'
-    
+
         model.DBSession.add(u)
-    
-        g = model.Group()
-        g.group_name = u'managers'
-        g.display_name = u'Managers Group'
-    
-        g.users.append(u)
-    
-        model.DBSession.add(g)
-    
-        p = model.Permission()
-        p.permission_name = u'manage'
-        p.description = u'This permission give an administrative right to the bearer'
-        p.groups.append(g)
-    
-        model.DBSession.add(p)
-    
+
         u1 = model.User()
         u1.user_name = u'editor'
         u1.display_name = u'Example editor'
         u1.email_address = u'editor@somedomain.com'
         u1.password = u'editpass'
-    
+
         model.DBSession.add(u1)
+
+        g = model.Group()
+        g.group_name = u'smokers'
+        g.display_name = u'Smokers Group'
+
+        g.users.append(u1)
+
+        model.DBSession.add(g)
+
+        p = model.Permission()
+        p.permission_name = u'edit'
+        p.description = u'This permission gives rights to add smoking data'
+        p.groups.append(g)
+
+        model.DBSession.add(p)
+
         model.DBSession.flush()
         transaction.commit()
     except IntegrityError:
