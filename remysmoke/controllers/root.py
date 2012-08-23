c2e1d31003dac88117f6150d0c0039b637c2dacc
Nathaniel Case <Qalthos@gmail.com>
2012-08-23 14:03:19 -0400

    Remove more page.

--- remysmoke/controllers/root.py
+++ remysmoke/controllers/root.py
@@ -216,8 +216,7 @@ class RootController(BaseController):
         login_counter = request.environ['repoze.who.logins']
         if login_counter > 0:
             flash(_('Wrong credentials'), 'warning')
-        return dict(page='login', login_counter=str(login_counter),
-                    came_from=came_from)
+        return dict(login_counter=str(login_counter), came_from=came_from)
 
     @expose()
     def post_login(self, came_from=lurl('/')):
