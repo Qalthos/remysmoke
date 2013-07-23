54f32ab3fbc7a537a207630de98d0d1d1d3b9ab0
Nathaniel Case <Qalthos@gmail.com>
2013-07-23 13:51:07 -0400

    Move the graphs to their own file too.

--- remysmoke/controllers/root.py
+++ remysmoke/controllers/root.py
@@ -12,7 +12,7 @@ from remysmoke.lib.base import BaseController
 from remysmoke.model import DBSession
 from remysmoke.model.smoke import Cigarette
 from remysmoke.model.unsmoke import Unsmoke
-from remysmoke.widgets import punch_chart, time_chart
+from remysmoke.widgets.graphs import punch_chart, time_chart
 from remysmoke.widgets.stats import smoke_stats
 
 __all__ = ['RootController']
